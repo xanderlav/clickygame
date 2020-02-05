bdd1980eb117fdf06b4e7b40736ae9b29dd1157a
@xanderlav <alejandroregino.hussmann.com>
2020-02-04 19:05:17 -0600

    Updates

---
+++ precache-manifest.f372f1c522bf4257594898945bbf59cc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbbf5ef81604739447b4c736e223c716",
+    "url": "/clickygame/index.html"
+  },
+  {
+    "revision": "f7c9d779132708ad3510",
+    "url": "/clickygame/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "fc5e2e816fbad40007cc",
+    "url": "/clickygame/static/js/2.1f831936.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/clickygame/static/js/2.1f831936.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f7c9d779132708ad3510",
+    "url": "/clickygame/static/js/main.1fef998b.chunk.js"
+  },
+  {
+    "revision": "4d95e025c74c73140b85",
+    "url": "/clickygame/static/js/runtime-main.2999711a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/clickygame/static/media/logo.5d5d9eef.svg"
+  }
+]);
